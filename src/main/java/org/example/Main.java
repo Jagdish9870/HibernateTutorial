b622126c043cb8f691a7beb201dd6adde1f3465c
jagdish gupta <varshneyjacky@gmail.com>
2024-12-04 21:35:01 +0530

    configuration of hibernate using xml and fetching database

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        Configuration cfg= new Configuration();
+        cfg.configure();
+        SessionFactory factory=cfg.buildSessionFactory();
+        System.out.println(factory);
     }
 }

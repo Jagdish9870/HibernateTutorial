5d34e962efa59066a5d23b8204b81529e92b8c6a
jagdish gupta <varshneyjacky@gmail.com>
2024-12-06 17:44:39 +0530

    configuring hibernate using java file

---
+++ src/main/java/org/example/HibernateUtil.java
@@ -0,0 +1,39 @@
+package org.example;
+// if dont want to use xml file...... we can use this java file.....
+import org.hibernate.SessionFactory;
+
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+
+public class HibernateUtil {
+    private static SessionFactory sessionfactory;
+    public static SessionFactory getSessionfactory(){
+        if(sessionfactory==null){
+            Configuration configuration =new Configuration();
+            Properties properties = getProperties();
+            configuration.setProperties(properties);
+            ServiceRegistry serviceRegistry=new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            sessionfactory= (SessionFactory) configuration.buildSessionFactory(serviceRegistry);
+        }
+        return sessionfactory;
+    }
+
+    private static Properties getProperties() {
+        Properties properties= new Properties();
+        properties.put(Environment.DRIVER,"com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.URL,"jdbc:mysql://localhost:3306/hibernate_db");
+        properties.put(Environment.USER,"root");
+        properties.put(Environment.PASS,"987068@Vjack");
+        properties.put(Environment.DIALECT,"org.hibernate.dialect.MySQLDialect");
+        properties.put(Environment.HBM2DDL_AUTO,"update");
+        properties.put(Environment.SHOW_SQL,true);
+        return properties;
+    }
+
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,29 @@
 package org.example;
 
-import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
 
-        Configuration cfg= new Configuration();
-        cfg.configure();
-        SessionFactory factory=cfg.buildSessionFactory();
+//       for xml file only
+//        Configuration cfg= new Configuration();
+//        cfg.configure();
+//        SessionFactory factory=cfg.buildSessionFactory();
+//
+//       // in short....
+//        // SessionFactory factory=new Configuration().configure().buildSessionFactory();
+//
+//
+//        System.out.println(factory);
+
+        // for java file(HibernateUtil)
+
+        SessionFactory factory= HibernateUtil.getSessionfactory();
         System.out.println(factory);
+
     }
 }
